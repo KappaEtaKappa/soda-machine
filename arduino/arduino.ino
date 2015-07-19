86d172f0d6c409e9b2db3df62d06600502b9723d
Lucas Mullens <lucas@whisper.sh>
2015-07-18 19:43:18 -0500

    adding debug info for valid/invalid id's

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -42,16 +42,10 @@ void ISRzero(void)
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
 
-// Set the static IP address to use if the DHCP fails to assign
+//set the server to delta
 IPAddress server(10,0,0,10);
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server 
-// that you want to connect to (port 80 is default for HTTP):
 EthernetClient client;
 
 void setup() {
@@ -149,6 +143,7 @@ void startConnection() {
     // kf you didn't get a connection to the server:
     Serial.println("Connection failed");
     clearAllBuffers();
+    startConnection();
   } 
 }
 void checkID(unsigned long id) {
@@ -156,6 +151,8 @@ void checkID(unsigned long id) {
   lines = 0;
   timeKeeper = 0;
   
+  Serial.println("Checking ID ");
+  Serial.println(id); 
   client.println(id);
 }
 void invalidID() {
